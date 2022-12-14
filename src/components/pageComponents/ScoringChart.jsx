31d636113d0a3d5d4f0b16bad94f2b7f72bf4326
cqb13 <cqb13.main@gmail.com>
2022-12-13 22:29:48 -0500

    📄 Auto scoring chart

--- src/components/pageComponents/ScoringChart.jsx
+++ src/components/pageComponents/ScoringChart.jsx
@@ -1,46 +1,64 @@
-import React from "react";
-import "../../css/ScoringChart.css";
+import React, { useState, useEffect } from 'react';
+import "../../css/ScoringChart.css"
 
-class ScoringChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arrows: this.props.arrowsPerEnd,
-      ends: this.props.ends
-    };
-  }
+function ScoringChart(props) {
+  const [data, setData] = useState([[]]);
+  //TODO: monkey code (new values from sliders are strings not ints)
+  const arrowsPerEnd = parseInt(props.arrowsPerEnd);
+  const ends = parseInt(props.ends);
 
-  // setup score map and chart
-  componentDidMount() {
-    let scoreMap = [];
-    let totalArrows = this.state.arrows * this.state.ends;
-    for (let i = 0; i < totalArrows; i++) {
-      scoreMap.push(0);
-    }
-    this.setState({scoreMap: scoreMap});
-
-    let chart = document.querySelector(".Scoring-Chart");
-    let section = document.createElement("div");
-    section.className = "Section";
-    for (let i = 0; i < this.state.ends; i++) {
-      for (let j = 0; j < this.state.arrows; j++) {
-        let newSection = section.cloneNode();
-        newSection.id = i + "-" + j;
-        newSection.innerHTML = parseInt(i + 1) + "-" + parseInt(j + 1);
-        chart.appendChild(newSection);
+  const handleChange = (event, rowIndex, columnIndex) => {
+    // Update the value of the text box at the specified row and column
+    const updatedData = data.map((row, rIndex) => {
+      if (rIndex === rowIndex) {
+        return row.map((column, cIndex) => {
+          if (cIndex === columnIndex) {
+            // Only allow numbers to be entered in the text box
+            return /^\d+$/.test(event.target.value) ? event.target.value : '';
+          }
+          return column;
+        });
       }
-    }
+      return row;
+    });
+    setData(updatedData);
+  };
 
-    this.componentDidMount = () => {};
-  }
+  useEffect(() => {
+    setData(Array.from(Array(ends), () => new Array(arrowsPerEnd).fill('')));
+  }, [arrowsPerEnd, ends]);
 
-  render() {
-    return (
-      <div className="Scoring-Chart">
-      
-      </div>
-    );
-  }
+  return (
+    <div className='Scoring-Chart'>
+      <table>
+        <thead>
+          <tr>
+            {data[0].map((_, columnIndex) => (
+              <th key={columnIndex}>Arrow {columnIndex + 1}</th>
+            ))}
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((column, columnIndex) => (
+                <td key={columnIndex}>
+                  <input
+                    type="text"
+                    value={column}
+                    onChange={event => handleChange(event, rowIndex, columnIndex)}
+                  />
+                </td>
+              ))}
+              <td>{row.reduce((a, b) => parseInt(a, 10) + parseInt(b, 10), 0)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ScoringChart;
+           
